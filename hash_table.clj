225e54642758c995b869c4b49823fb07343e1d11
Benny Tsai <benny.tsai@gmail.com>
2011-07-17 21:02:31 -0600

    implement simple chaining hash table

---
+++ hash_table.clj
@@ -0,0 +1,28 @@
+(ns hash-table)
+
+(def m 256)
+
+(defn hash-table []
+  (vec (repeat m '())))
+
+(defn hash-key [x]
+  (rem (hash x) m))
+
+(defn search [table x]
+  (let [k (hash-key x)
+        bucket (nth table k)]
+    (some #{x} bucket)))
+
+(defn insert [table x]
+  (let [k (hash-key x)
+        bucket (nth table k)]
+    (if (some #{x} bucket)
+      table
+      (assoc table k (conj bucket x)))))
+
+(defn delete [table x]
+  (let [k (hash-key x)
+        bucket (nth table k)]
+    (if (some #{x} bucket)
+      (assoc table k (remove #{x} bucket))
+      table)))
