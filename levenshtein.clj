ba837317f67b4c6d4dc8dc969e9f01ff994b2f7e
Benny Tsai <benny.tsai@gmail.com>
2011-11-15 00:38:28 -0700

    replace imperative Levenshtein with optimized version of pure functional code from lpetit

--- levenshtein.clj
+++ levenshtein.clj
@@ -1,23 +1,19 @@
 (ns levenshtein)
 
-;; http://en.wikipedia.org/wiki/Levenshtein_distance#Computing_Levenshtein_distance
-(defn levenshtein [^String s ^String t]
-  (let [m (count s)
-        n (count t)
-        ^objects d (make-array Integer/TYPE (inc m) (inc n))]
-    (doseq [i (range (inc m))]
-      (aset-int d i 0 i))
-    (doseq [j (range (inc n))]
-      (aset-int d 0 j j))
-    (doseq [j (range 1 (inc n))
-            i (range 1 (inc m))]
-      (let [i-1 (dec i)
-            j-1 (dec j)
-            ^ints row-i (aget d i)
-            ^ints row-i-1 (aget d i-1)]
-        (aset-int row-i j (if (= (.charAt s i-1) (.charAt t j-1))
-                            (aget row-i-1 j-1)
-                            (inc (min (aget row-i-1 j)
-                                      (min (aget row-i j-1)
-                                           (aget row-i-1 j-1))))))))
-    (aget d m n)))
+;; Laurent Petit's code from below with small optimization
+;; https://gist.github.com/828413
+(defn- new-row [prev-row row-elem t]
+  (reduce 
+   (fn [row [d-1 d e]]
+     (conj row (if (= row-elem e)
+                 d-1
+                 (inc (min (peek row)
+                           (min d d-1)))))) ;; make all mins binary calls
+   [(inc (first prev-row))]
+   (map vector prev-row (next prev-row) t)))
+
+(defn levenshtein [s t] 
+  (peek (reduce 
+         (fn [prev-row s-elem] (new-row prev-row s-elem t)) 
+         (range (inc (count t))) 
+         s)))
