17bf70b9ec94f4630dd0f8742cf0fda68bc68dbe
Benny Tsai <benny.tsai@gmail.com>
2012-03-03 11:58:40 -0800

    add (vec) so that empty s doesn't blow us up, reformat

--- levenshtein.clj
+++ levenshtein.clj
@@ -3,17 +3,16 @@
 ;; Laurent Petit's code from below with small optimization
 ;; https://gist.github.com/828413
 (defn- new-row [prev-row row-elem t]
-  (reduce 
-   (fn [row [d-1 d e]]
-     (conj row (if (= row-elem e)
-                 d-1
-                 (inc (min (peek row)
-                           (min d d-1)))))) ;; make all mins binary calls
-   [(inc (first prev-row))]
-   (map vector prev-row (next prev-row) t)))
+  (reduce (fn [row [d-1 d e]]
+            (conj row (if (= row-elem e)
+                        d-1
+                        (inc (min (peek row) ;; use only binary min calls
+                                  (min d d-1))))))
+          [(inc (first prev-row))]
+          (map vector prev-row (next prev-row) t)))
 
 (defn levenshtein [s t] 
-  (peek (reduce 
-         (fn [prev-row s-elem] (new-row prev-row s-elem t)) 
-         (range (inc (count t))) 
-         s)))
+  (peek (reduce (fn [prev-row s-elem]
+                  (new-row prev-row s-elem t)) 
+                (vec (range (inc (count t)))) 
+                s)))
